be72ca323c15aff850e70242aeaf3fe8e84f3343
Nicolas Gidon <nicolas.gidon@gmail.com>
2019-09-24 00:34:52 +0200

    init project

---
+++ TestApi/TestApi/Controllers/TodoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestApi.Models;
+
+namespace TestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetTodoItems()
+        {
+            return _context.TodoItem.ToList();
+        }
+    }
+}
---
+++ TestApi/TestApi/Models/TodoContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItem{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TodoItem>(entity =>
+            {
+                entity.Property(e => e.Id).HasComputedColumnSql("id");
+                entity.Property(e => e.Name).HasComputedColumnSql("name");
+                entity.Property(e => e.IsComplete).HasComputedColumnSql("is_complete");
+
+            });
+        }
+    }
+}
